9cc71d180eb5f87b110c6adc8b459d483d2579ab
puriwat.l <puriwat.l@rmutsv.ac.th>
2022-09-12 10:27:28 +0700

    Lab 6: example mid exam

--- Lab6/mypet.py
+++ Lab6/mypet.py
@@ -17,9 +17,3 @@ class MyPet:
     def display_info(self):
         print(f'Name: {self.name} Age: {self.age} Breed: {self.breed}')
 
-    def display_all_mypet(self):
-        if len(self.mypet) ==0:
-            print("You had no pet.")
-        else:
-            for x in self.mypet:
-                x.display_info()
--- Lab6/mypet_app.py
+++ Lab6/mypet_app.py
@@ -1,25 +1,60 @@
-from ex_class2 import MyPet
+from mypet import MyPet
 
 
-def test_fun():
-    MyPet.display_all_mypet("")
+# ใช้แสดงตัวเลือกให้ผู้เลือกการทำงาน
+"""
+1.เพิ่มข้อมูลสัตว์เลี้ยง
+2.แสดงข้อมูลสัตว์เลี้ยง
+3.ออกจากระบบ
+"""
+def display_option():
+    print("\nPlease, select option below: ")
+    print("1. add pet data.")
+    print("2. display pets data.")
+    print("3. exit.")
 
-def input_pet_data():
+    b = True
+    while b:
+        select = int(input("select(1-3)? :"))
+        if select ==1:
+            input_mypet_data()
+            display_option()
+        elif select ==2:
+            display_mypet()
+            display_option()
+        elif select ==3:
+            print("Good Bye.")
+            exit(0)
+        else:
+            print("Please, enter 1-3 only. Thank.\n")
+
+
+# ใช้แสดงข้อมูลของสัตว์เลี้ยงทั้งหมด
+def display_mypet():
+    if len(mypet_list) ==0:
+        print("You had no pet data.")
+    else:
+        count = 1
+        print(f"Your had {len(mypet_list)} pet following: ")
+        for x in mypet_list:
+            print(f'{count}.',end=" ")
+            f'{x.display_info()}'
+            count += 1
+
+# ใช้รับข้อมูลสัตว์เลี้ยงจากผู้ใช้
+def input_mypet_data():
     name = input("Enter your pet name: ")
     age = int(input("Enter your pet age: "))
     breed = input("Enter your pet breed: ")
+    mypet_list.append(MyPet(name,age,breed))
+    print("Your pet data is already add to store....")
 
-    return name,age,breed
 
-data = input_pet_data()
-p1 = MyPet(data[0],data[1],data[2])
-p1.display_info()
+# list
+print("---------------------------------------")
+print("|  Welcome, to Pet data store system  |")
+print("---------------------------------------")
+mypet_list = []
+display_option()
 
-data = input_pet_data()
-p2 = MyPet(data[0],data[1],data[2])
-p2.display_info()
 
-print(f"I had {len(MyPet.mypet)} following: ")
-
-#MyPet.display_all_mypet()
-test_fun()
